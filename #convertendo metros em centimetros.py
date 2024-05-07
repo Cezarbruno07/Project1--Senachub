2bcccc76734488aff00fea7e830c76979f9f402a
Cezarbruno07 <165038823+Cezarbruno07@users.noreply.github.com>
2024-05-07 09:52:12 -0400

    Create #convertendo metros em centimetros.py

---
+++ #convertendo metros em centimetros.py
@@ -0,0 +1,4 @@
+#convertendo metros em centimetros
+metros = float(input('Digite o tamando :'))
+print ('O Valor{}m em centimetro é {:0f}cm '.format(metros,metros *100))
+print ('O Valor{}m em centimetro é {:0f}cm '.format(metros,metros *1000))
