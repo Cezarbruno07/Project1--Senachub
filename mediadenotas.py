261e076fa886834c35963efd500f0ccec4addfd2
Cezarbruno07 <165038823+Cezarbruno07@users.noreply.github.com>
2024-05-07 08:06:17 -0400

    Create mediadenotas.py

---
+++ mediadenotas.py
@@ -0,0 +1,14 @@
+nota1=float(input(" Digite a nota do Primeiro bimestre "))
+nota2=float(input(" Digite a nota do segundo bimestre "))
+nota3=float(input(" Digite a nota do terceiro bimestre "))
+nota4=float(input(" Digite a nota do Quarto bimestre "))
+media = (nota1+nota2+nota3+nota4)/4
+print(" a NOta Primeiro bimestre é : ",nota1)
+print(" a NOta Primeiro bimestre é : ",nota2)
+print(" a NOta Primeiro bimestre é : ",nota3)
+print(" a NOta Primeiro bimestre é : ",nota4)
+
+if media >=7:
+    print("Aprovado")
+else :
+    print("Não aprovado")
