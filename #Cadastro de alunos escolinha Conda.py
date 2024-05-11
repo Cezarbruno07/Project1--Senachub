13da3a6a44608e08222b4871f429b294d9fa354e
Bruno Cezar <165038823+Cezarbruno07@users.noreply.github.com>
2024-05-10 21:46:58 -0300

    5

--- #Cadastro de alunos escolinha Conda.py
+++ #Cadastro de alunos escolinha Conda.py
@@ -1,22 +1,18 @@
 
 #Cadastro de alunos escolinha Conda 
 import sqlite3
+
 banco = sqlite3.connect('Cadastrodealunos.db')
+
 cursor = banco.cursor()
 
-#cursor.execute("CREAT TABLE Alunos(Nome text,idade integer,email text)")
-cursor.execute("INSERT INTO alunos VALUES(,Maria,40,'Maria_123@gmail.com')")
-banco.commit()
+cursor.execute("CREAT TABLE alunos(Nome text,idade integer,email text)")
+
+cursor.execute("INSERT INTO alunos VALUES('bruno',40,'bruno_123@gmail.com)")
+
+banco.commit('cadastrodealunos.db')
+
 cursor.execute("SELECT *FROM pessoas")
 
 print(cursor.fetchall())
 
-""" na= str(input("Nome do aluno = "))
-ia=int(input("Idade do aluno = "))
-nr=str(input("Nome do Responsalvel = "))
-
-
-
-print ('Nome do aluno = ',na)
-print ('Idade do aluno = ',ia) 
-print ('Nome do responsavel =',nr) """ 
