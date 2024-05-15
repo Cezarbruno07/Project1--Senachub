da5950c07ed7e12016fdf6c4100ed7dc75ac98eb
Cezarbruno07 <165038823+Cezarbruno07@users.noreply.github.com>
2024-05-15 08:19:55 -0400

    12

--- Auladelistas15-05.py
+++ Auladelistas15-05.py
@@ -8,19 +8,31 @@ print (len(lista1))
 lista1 = ["oi",2.0,5,[10,20]]
 print (len(lista1))
  """
-""" numeros = ["oi",123,87,34,66,8398,44,"Bom dia"]
+
+""" #Imprima os itens da lista 
+numeros = ["oi",123,87,34,66,8398,44,"Bom dia"]
 print(numeros[0])
 print(numeros[9-8])
 print(numeros[-2])
 print(numeros[-4])
 print(numeros[5])
-print(numeros[7]) """
+print(numeros[7])  """
 
-
+""" #Selecione numero dentro da lista 
 lista1=[3,67,"gato",[56,57,"cachorro"],[],3.14,False]
-print(lista1[2][0])
+print(lista1[2][3]) """
 
+""" #Encontre dados dentro das tabelas 
+frutas=["maca","laranja","Banana","Cereja"]
 
+print("maca" in frutas)
+print("Pera" in frutas) """
 
+#Contatenar 
+frutas=["maca","laranja","Banana","Cereja"]
+print([1,2]+[3,4])
+print(frutas + [6,7,8,9])
 
+print(["teste"]*9)
+print([1,2,["ola","adeus"]]*2)
 
