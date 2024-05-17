e75ab7dd8207d2a1bd81aa367b670ac557d44cf1
Cezarbruno07 <165038823+Cezarbruno07@users.noreply.github.com>
2024-05-17 07:57:49 -0400

    aula07-15

---
+++ aula17-05Tabelas.py
@@ -0,0 +1,5 @@
+#Escreva funcao chamada que receba uma lista de listas de numeros inteiro adicione 
+#todos elementos de todas as listas aninhadas soma 
+t=[[1,2],[3],[4,5,6]]
+
+print(t[0][0]+t[0][1]+t[1][0]+t[2][0]+t[2][1]+t[2][2])
