e956500816580ba325f67a1ef7aa0752d6d24270
Cezarbruno07 <165038823+Cezarbruno07@users.noreply.github.com>
2024-05-29 09:06:31 -0400

    09-06

--- #Imprima 5 numeros e informe a soma e me.py
+++ #Imprima 5 numeros e informe a soma e me.py
@@ -1,22 +1,22 @@
-#Imprima 5 numeros e informe a soma e media dos numeros
-numeros=int(input("Quantos numeros: "))
+#Aula 29/5/2024 media e soma de numeros
+num=int(input("Quantos numeros: "))
 
-primeiro=int(input("Numero 1: "))
+inicial=int(input("Numero 1: "))
 
 count=1
-maior=primeiro
-soma=primeiro
+maior=inicial
+soma=inicial
 
-while count< numeros:
+while count< num:
     count += 1
     temp=int(input("Numero %d: " % count))
     soma += temp
     if temp>maior:
         maior = temp
     
-media = soma / numeros
+media = soma / num
 print("Soma:",soma)
 print("Maior:", maior)
-print("Media: %.2f" % (soma/numeros))
+print("Media: %.2f" % (soma/num))
 
 
