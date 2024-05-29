486b718486f0385ad0df30a3b5bed9a41c781289
Bruno Cezar <165038823+Cezarbruno07@users.noreply.github.com>
2024-05-28 21:28:19 -0300

    1

--- #Cadastro de alunos escolinha Conda.py
+++ #Cadastro de alunos escolinha Conda.py
@@ -1,4 +1,5 @@
 import sqlite3
+<<<<<<< Updated upstream
 nome=str(input("Digite seu nome = "))
 idade=int(input("Qual idade = "))
 email=bool(input("Digite seu email = "))
@@ -17,3 +18,18 @@ banco.commit('cadastrodealunos.db')
 #cursor.execute("SELECT *FROM alunos")
 
 #print(cursor.fetchall()) 
+=======
+banco = sqlite3.connect('Cadastro_de_alunos.db')
+""" cursor = banco.cursor()
+
+#cursor.execute("CREAT TABLE Alunos(Nome text,idade integer,email text)")
+cursor.execute("INSERT INTO alunos VALUES('Maria',40,'Maria_123@gmail.com')")
+
+
+cursor.execute("INSERT INTO  pessoas")
+
+banco.commit() """
+
+
+
+>>>>>>> Stashed changes
