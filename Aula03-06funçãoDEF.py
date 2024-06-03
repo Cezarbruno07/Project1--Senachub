8bd0ae29c6d88afa4eb9962ecb9d19597648e21e
Cezarbruno07 <165038823+Cezarbruno07@users.noreply.github.com>
2024-06-03 10:13:59 -0400

    55

---
+++ Aula03-06funçãoDEF.py
@@ -0,0 +1,38 @@
+# def hello():
+#     print("Ola!!!")
+    
+# hello()
+
+
+# def hello (nome):
+#     print("Ola", nome)
+# hello ("Ederson")
+
+
+# def hello(nome):
+#     print("Seja Bem - Vindo",nome)
+
+# a=input("Digite seu nome = ")
+
+# hello(a)
+
+
+# def hello (nome):
+#     print("Ola",nome)
+
+# hello ('Ederson')
+# hello ('Bruno')
+# hello ('Cezar')
+
+# def fareheit(temp):
+#     return ((temp-32) *(5/9))    
+
+                    
+def hello (nome , idade ):
+    print("ola" ,nome ,'\n Sua idade é :', idade)
+
+
+hello ("Ederson", 25)
+
+
+
