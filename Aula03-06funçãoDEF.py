505c6cc7aca2b3de23827938bd38ceb7edd40a8f
Cezarbruno07 <165038823+Cezarbruno07@users.noreply.github.com>
2024-06-03 10:31:17 -0400

    Update Aula03-06funçãoDEF.py

--- Aula03-06funçãoDEF.py
+++ Aula03-06funçãoDEF.py
@@ -28,11 +28,23 @@
 #     return ((temp-32) *(5/9))    
 
                     
-def hello (nome , idade ):
-    print("ola" ,nome ,'\n Sua idade é :', idade)
+# def hello (nome , idade ):
+#     print("ola" ,nome ,'\n Sua idade é :', idade)
 
 
-hello ("Ederson", 25)
+# hello ("Ederson", 25)
+
+def calcular_pagamento(qtd_horas,valor_hora):
+    horas=float(qtd_horas)
+    taxa=float(valor_hora)
+    if horas<=40:
+        salario=horas*taxa
+    else:
+        h_excd = horas - 40
+        salario = 40*taxa+(h_excd*(1.5*taxa))
+    print(salario)
 
 
-
+hora = input("Digite horas ")
+valor= int(input("Digite o Valor da hora"))
+calcular_pagamento(hora,valor)
