d5721d28f609d9b9d1644ae336528469cb599e63
Bruno Cezar <165038823+Cezarbruno07@users.noreply.github.com>
2024-06-07 20:48:11 -0300

    2048

--- Tarefa3-cisco.py
+++ Tarefa3-cisco.py
@@ -1,12 +1,12 @@
 def is_year_leap(year):
     if year %4 !=0:
         return False
-elif year % 100!=0:
-    return True
-elif year $400 !=0:
-    return False
-else:
-    return True
+    elif year % 100!=0:
+        return True
+    elif year % 400 !=0:
+        return False
+    else:
+        return True
 
 def days_in_month(year, month):
     if year < 1582 or month < 1 or month >12:
