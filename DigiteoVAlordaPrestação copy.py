cd3aa9d84edb28061eb97871417bc68db79ce40e
Cezarbruno07 <165038823+Cezarbruno07@users.noreply.github.com>
2024-06-12 11:02:43 -0400

    aw

--- DigiteoVAlordaPrestação copy.py
+++ DigiteoVAlordaPrestação copy.py
@@ -1,4 +1,4 @@
-def valorPagamento(valor, dias):
+def valorPagamento(valorPagamento,total):
     juros = 0
     if dias == 0:
         return valor
@@ -7,19 +7,22 @@ def valorPagamento(valor, dias):
 
         return valor + ((3 * valor)/100) + juros 
 
-total = 0
-cont = 0
-while True:
-    valor = float(input('Digite o valor da prestação: '))
-    if valor == 0:
-        break
-    dias = int(input('Digite o número de dias em atraso: '))
-    print (valorPagamento(valor, dias))
-    total += valorPagamento(valor, dias)
-    cont += 1
 
-print('Quantidade total: %d' % cont)
-print('Valor total das prestações: %.2f' % total)
+
+
+#total = 0
+#cont = 0
+#while True:
+    #valor = float(input('Digite o valor da prestação: '))
+    #if valor == 0:
+        #break
+    #dias = int(input('Digite o número de dias em atraso: '))
+    #print (valorPagamento(valor, dias))
+    #total += valorPagamento(valor, dias)
+    #cont += 1
+
+#print('Quantidade total: %d' % cont)
+#print('Valor total das prestações: %.2f' % total)
 
 
 
