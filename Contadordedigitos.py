d12a911d6643db9783adf4f513cb2bb405283ec8
Cezarbruno07 <165038823+Cezarbruno07@users.noreply.github.com>
2024-06-17 07:30:10 -0400

    17-06

---
+++ Contadordedigitos.py
@@ -0,0 +1,8 @@
+def Con_digito(n):
+    return len(str(n))
+def exibe():
+    n = int(input('Digite uma Sequencia de numeros ='))
+    print(Con_digito(n), ' Dígitos')
+    
+while True:
+    exibe()
