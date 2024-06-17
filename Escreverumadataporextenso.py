0bcfc369fc8ed5772ae717543872dd4c9c1b037c
Cezarbruno07 <165038823+Cezarbruno07@users.noreply.github.com>
2024-06-17 08:42:17 -0400

    Update Escreverumadataporextenso.py

--- Escreverumadataporextenso.py
+++ Escreverumadataporextenso.py
@@ -1,10 +1,14 @@
+def data(data):
+    if data [3:5]== "01":
+        print(f"{data[0:2]} de janeiro de {data[6:]}")
+        
+data("01/01/2024")
+
+
 meses = ["janeiro","fevereiro","março","abril","maio","junho","julho","agosto","setembro","outubro","novembro","dezembro"]
+
 
 data = input("informe a data (dd/mm/aaaa): ")
 
-
-print (data.split("/")[0],
-       "de",
-       meses[(int(data.split("/")[1])-1)],
-       "de",
-       data.split("/")[2])
+print (data.split("/")[0],"de",meses[(int(data.split("/")[1])-1)],"de",data.split("/")[2])
+ 
